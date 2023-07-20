ed19dadb447548954813ddb6997f1cf72937f425
houssam72 <brothersmarket72@gmail.com>
2023-07-21 01:24:43 +0200

    Add sideBar And Redux , youtub data api v3 config

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import { useEffect } from "react";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { getHomePageVideos } from "../store/reducers/getHomePageReduce";
 
 type Props = {};
 
 const Home = (props: Props) => {
+  const dispatch = useAppDispatch();
+  const videos = useAppSelector((state) => state.youtubeApp.videos);
+
+  useEffect(() => {
+    dispatch(getHomePageVideos(false));
+    console.log("Test", videos);
+  },[dispatch]);
   return (
-    <div>
-      <Navbar />
+    <div className="max-h-screen overflow-hidden ">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+      </div>
     </div>
   );
 };
